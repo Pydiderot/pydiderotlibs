c3ead10e4cdfdbbdfe042ca48eb502de2ef18618
cspaier <clement.spaier@gmail.com>
2019-09-14 10:07:32 +0200

    maj 0.0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ for line in open(os.path.join(setupdir, 'requirements.txt'), encoding="UTF-8"):
 
 setup(
     name='pydiderotlibs',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     packages_dir = {'' : 'pydiderotlibs'},
     author='Professeurs de Mathématiques du lycée Denis Diderot (Marseille)',
