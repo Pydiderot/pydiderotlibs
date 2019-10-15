281c325891bbbeee43166e9e8ae7d2430b956398
cspaier <clement.spaier@gmail.com>
2019-10-15 21:15:16 +0200

    maj 0.0.8 pour pypi

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ for line in open(os.path.join(setupdir, 'requirements.txt'), encoding="UTF-8"):
 
 setup(
     name='pydiderotlibs',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     packages_dir = {'' : 'pydiderotlibs'},
     author='Professeurs de Mathématiques du lycée Denis Diderot (Marseille)',
