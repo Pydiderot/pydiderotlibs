cbb419543ce60692eb905e58a0dd85f8bc3d90a5
byache <byache@users.noreply.github.com>
2019-12-04 10:58:01 +0100

    Update math_lycee.py

--- pydiderotlibs/math_lycee.py
+++ pydiderotlibs/math_lycee.py
@@ -29,20 +29,24 @@ pi = math.pi
 
 
 def repeter(f, n):
-    """ Appelle `n` fois la fonction ``f``.
+    """ 
+    Appelle `n` fois la fonction ``f``.
 
     Alias disponible: ``repeat()``
     """
     for i in range(n):
         f()
 
-
 def repeat(f, n):
     repeter(f, n)
     
 def intersect(a1,b1,a2,b2):
-    """ Détection d'intersection entre deux intervalles : renvoie `false` si l'intersection de [a1;a2] et [b1;b2] est vide.
+    """ 
+    Détection d'intersection entre deux intervalles : renvoie `false` si l'intersection de [a1;a2] et [b1;b2] est vide.
     
+    Arguments:
+        a1 et b1 (float): bornes du premier intervalle
+        a2 et b2 (float): bornes du second intervalle
     """
     if b1<a2 or b2<a1:
         return False
@@ -50,7 +54,15 @@ def intersect(a1,b1,a2,b2):
         return True
     
 def collision(x1,y1,w1,h1,x2,y2,w2,h2):
-    """renvoie "true" si les deux rectangles se rencontrent
+    """
+    Détection d'intersection entre deux rectangles : 
+    renvoie `true` si les deux rectangles se rencontrent
+    
+    Arguments:
+        x1 et y1 (float): coordonnées du centre du premier rectangle
+        w1 et h1 (float): largeur et hauteur du premier rectangle
+        x2 et y2 (float): coordonnées du centre du second rectangle
+        w2 et h2 (float): largeur et hauteur du second rectangle
     """
     if( intersect(x1-w1/2,x1+w1/2,x2-w2/2,x2+w2/2) and intersect(y1-h1/2,y1+h1/2,y2-h2/2,y2+h2/2) ):
         return True
