b4447f852d44d8cb26559250a79681a018273778
cspaier <clement.spaier@gmail.com>
2019-12-15 12:03:41 +0100

    version 0.0.14

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@
 Ce module contient les librairies utilisées par les enseignents du lycée Denis Diderot pour enseigner python.
 """
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 #from .lycee import *
 #from .graphique import *
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ for line in open(os.path.join(SETUPDIR, 'requirements.txt'), encoding="UTF-8"):
 
 setup(
     name='pydiderotlibs',
-    version='0.0.13',
+    version='0.0.14',
     packages=find_packages(),
     packages_dir={'' : 'pydiderotlibs'},
     author='Professeurs de Mathématiques du lycée Denis Diderot (Marseille)',
