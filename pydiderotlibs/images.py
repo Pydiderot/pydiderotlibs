fdca085e8dd611ff4fa72cb0ae3b58594345fea6
byache <byache@users.noreply.github.com>
2020-03-10 15:39:18 +0100

    documentation et qqs nouvelles fonctions

--- pydiderotlibs/images.py
+++ pydiderotlibs/images.py
@@ -1,42 +1,89 @@
 from PIL import Image
 from pydiderotlibs.couleurs import *
 
-#img = Image.open('lena.png').convert('LA')
-#img.save('greyscale.png')
-
 def creer_image(fichier,nom):
+    """
+    Crée, à partir d'une image enregistrée sur l'ordinateur,
+    une image manipulable dans le langage python (avec la librairie PIL)
+    
+    Arguments :
+        fichier : nom du fichier enregistré sur l'ordinateur (avec le suffixe : par exemple "monimage.png")
+        nom : le nom qu'aura l'image manipulable sous Python
+        
+    """
     nm=nom.split('.')
     if len(nm)==1:
         nom+='.png'
     nom = Image.open(fichier)
     
 def definition_image(image):
+    """
+    Retourne la définition de l'image ``image``.
+    C'est une liste de deux nombres.
+    Le premier est la largeur (en pixels), le second la hauteur (en pixels).
+    
+    Par exemple : [200,300]
+        
+    """
     return image.size
 
+def largeur_image(image):
+    """
+    Retourne la largeur de l'image ``image`` (en pixels).
+        
+    """
+    return image.size[0]
 
-#w,h=img.size
-#r,v,b=img.getpixel((100,250))
-#print(r)
-#print(img.getpixel((101,250)))
-#img.putpixel((100,250),(255,0,0))
-#img.putpixel((102,250),(255,0,0))
-#img.show()
+def hauteur_image(image):
+    """
+    Retourne la hauteur de l'image ``image`` (en pixels).
+        
+    """
+    return image.size[1]
+
+
 def afficher_image(image):
+    """
+    Affiche ``image`` (attention : sans l'enregistrer !).
+        
+    """
     image.show()
 
 def afficher_pixel(pixel):
+    """
+    Affiche une petite image unie, de 10x10 pixels, de la couleur du pixel ``pixel``.
+        
+    """
     Image.new('RGB', (10,10), pixel).show()
 
 def rouge(pixel):
+    """
+    Donne la valeur de rouge du pixel ``pixel``.
+        
+    """
     return pixel[0]
 
 def vert(pixel):
+    """
+    Donne la valeur de vert du pixel ``pixel``.
+        
+    """
     return pixel[1]
 
 def bleu(pixel):
+    """
+    Donne la valeur de bleu du pixel ``pixel``.
+        
+    """
     return pixel[2]
 
 def pixel_voisin(coord, image):
+    """
+    Donne les coordonnées du pixel situé à droite du pixel ``pixel``.
+    (S'il n'y a pas de pixel plus à droite car on est au bord de l'image,
+    retourne les mêmes coordonnées.)
+        
+    """
     x,y=coord
     xmax,ymax=image.size
     if x<xmax-1 :
@@ -45,24 +92,50 @@ def pixel_voisin(coord, image):
         return x,y
 
 def copier_pixel(image,coord):
+    """
+    Retourne le pixel de l'image ``image``, situé aux coordonnées ``coord``.
+        
+    """
     image.getpixel(coord)
 
-def coller_pixel(image,coord):
-    image.putpixel(coord)
+def coller_pixel(image,coord,pixel):
+    """
+    Remplace le pixel de l'image ``image``, situé aux coordonnées ``coord``, par le pixel ``pixel``.
+        
+    """
+    image.putpixel(coord,pixel)
 
 def enregistrer_image(image,nom):
+    """
+    Enregistre l'image ``image``, avec le nom de fichier indiqué.
+    
+    ``nom`` : nom du fichier avec extension, par exemple "monimage.png"
+        
+    """
     nm=nom.split('.')
     if len(nm)==1:
         nom+='.png'
-    image.save(nom)
-
-#pixel=rgb('orange')
-#afficher_pixel(pixel)
-#enregistrer_image(img,'essai')
-    
-
+    image.save(nom) 
     
 def changer_les_pixels(image, fonction, x0=0, x1=0, y0=0, y1=0):
+    """
+    Modifie les pixels de l'image ``image``, en leur appliquant la fonction ``fonction``.
+    
+    Arguments :
+        image : nom de l'image
+        
+        fonction : fonction à appliquer aux pixels. Ce doit être une fonction qui prend un pixel en argument
+        et qui retourne un pixel
+        
+        x0 (optionnel) : valeur minimale de la première coordonnée des pixels à modifier (0 par défaut)
+        
+        x1 (optionnel) : valeur maximale de la première coordonnée des pixels à modifier (0 par défaut, ce qui signifie que les pixels seront modifiés jusqu'à xmax)
+        
+        y0 (optionnel) : valeur minimale de la deuxième coordonnée des pixels à modifier (0 par défaut)
+        
+        y1 (optionnel) : valeur maximale de la deuxième coordonnée des pixels à modifier (0 par défaut, ce qui signifie que les pixels seront modifiés jusqu'à ymax)
+    
+    """
     if x1==0:
         x1=image.size[0]
     if y1==0:
