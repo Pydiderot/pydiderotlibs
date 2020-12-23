39dcbb047fda87c3b7169b3ee51ee5df2e80caba
JordanEmme <jordanemme@protonmail.com>
2020-12-23 18:50:28 +0000

    Implémentation des fonctions bezout et puissance_mod

--- pydiderotlibs/arithmetique.py
+++ pydiderotlibs/arithmetique.py
@@ -43,3 +43,43 @@ def quotient(a, b):
         b (int): Un nombre entier non nul.
     """
     return a // b
+
+
+def bezout(a,b):
+    """Renvoie un triplet d'entiers (d,u,v) tel que u*a + b*v = d = pgcd(a,b).
+
+    Arguments:
+        a (int) : un nombre entier
+        b (int) : un nombre entier
+    """
+    if b == 0 and a == 0 :
+        raise ZeroDivisionError(
+            "Le PGCD de deux nombres nuls n'existe pas")
+    r0 , r1 = a, b
+    u0, v0 = 1, 0
+    u1, v1 = 0, 1
+    while r1:
+        print(u0 * a + v0 * b)
+        q = r0 // r1
+        u0, u1 = u1, u0 - q*u1
+        v0, v1 = v1, v0 - q*v1
+        r0, r1 = r1, r0 - q*r1
+    return(r0,u0,v0)
+
+
+def puissance_mod(n,p,m):
+    """Renvoie n^p modulo m.
+    
+    Arguments:
+        n (int) : un nombre entier
+        p (int) : un nombre entier
+        m (int) : un nombre entier   
+    """
+    p_bin = bin(p)[-1:1:-1]
+    res = 1
+    carres = n % m
+    for exposant in p_bin:
+        if exposant == '1':
+            res = (res * carres) %  m 
+        carres = (carres ** 2) % m
+    return res   
