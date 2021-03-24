eb565d06a923fc6840efdb0f80b9b56d76c5e099
byache <paul@byache.fr>
2021-03-24 10:42:10 +0100

    maj version 0.0.20

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@
 Ce module contient les librairies utilisées par les enseignents du lycée Denis Diderot pour enseigner python.
 """
 
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 
 #from .lycee import *
 #from .graphique import *
